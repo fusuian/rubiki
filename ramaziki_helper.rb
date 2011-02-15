1e9a71104380ec6146a36974a06c9a06eb270db9
ASAHI,Michiharu <fusuian@gmail.com>
2011-02-16 00:26:11 +0900

    jQuery Mobile を導入。

--- ramaziki_helper.rb
+++ ramaziki_helper.rb
@@ -1,17 +1,50 @@
 # -*- coding: utf-8 -*-
 require 'ramaze'
+require 'ramaze/helper/link'
 
 module Ramaze
   module Helper
     module RamazikiHelper
-      def header
-        title = %Q(<div id="title_small">Rubuki</div>)
-        menus = [a("Home", :index), a("ページ（新着順）", :changes), a("ページ（ABC順）", :list), a("新規ページ作成", :create)]
-        %Q(<div id="header">\n#{title + menus*" | "}\n</div>)
+      def menus
+        [
+        a("Home", :index), 
+        a("新着", :changes), 
+        a("リスト", :list), 
+        a("新規", :create)
+        ]
+      end
+      
+      # jQuery mobile のナビゲーションバー
+      def navbar
+        <<EOS
+                <div data-role="navbar">
+          <ul>
+          #{ menus.map { |m| "<li>#{m}</li>" } * "\n" }
+          </ul>
+          </div>
+EOS
       end
       
       def footer
         %Q(<div id="footer"><hr/><div id="powered">Powered by Ruby #{RUBY_VERSION}</div></div>)
+      end
+      
+      # Innate::Helper::Link#a をコピペ。
+      # jQuery mobile のために rel="external" が必要だった。
+      def a(text, *args)
+        case first = (args.first || text)
+        when URI
+          href = first.to_s
+        when /^\w+:\/\//
+          uri = URI(first)
+          uri.query = Rack::Utils.escape_html(uri.query)
+          href = uri.to_s
+        else
+          href = args.empty? ? r(text) : r(*args)
+        end
+
+        text = Rack::Utils.escape_html(text)
+        %(<a href="#{href}" rel="external">#{text}</a>)
       end
     end
   end
